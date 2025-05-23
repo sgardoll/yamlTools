f46b88c24021753ae442946e321bfac7d532ae5f
Stuart <sgardoll@gmail.com>
2025-05-23 14:37:19 +1000

    feat: deploy Flutter build directly to root for GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3176965912"
+    serviceWorkerVersion: "3597845540"
   }
 });
